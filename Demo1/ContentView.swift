e8adea191ae174318f1445d2e07897f4afe62b2b
Susruthad <susruthanewid@gmail.com>
2024-01-10 19:59:32 -0800

    Initial Commit

---
+++ Demo1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Demo1
+//
+//  Created by user240733 on 1/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Demo1/Demo1App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo1App.swift
+//  Demo1
+//
+//  Created by user240733 on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
